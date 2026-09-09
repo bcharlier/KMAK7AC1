64f4acf70f2965957e737e36ac9253a2852f7bc7
Benjamin Charlier <benjamin.charlier@inrae.fr>
2026-09-09 12:55:50 +0200

    Add SVD courses

--- R/functions.R
+++ R/functions.R
@@ -1,4 +1,14 @@
-nice_print <- function(x) {
-  #knitr::kable(x)
-  return(x)
-} 
+print_table <- function(data, digits = 3, caption = "") {
+  if (digits >= 0) {
+    data <- round(data, digits)
+  }
+
+  align <- c("l", rep("c", ncol(data)))
+  knitr::kable(
+    data,
+    format = "html",
+    align = align,
+    caption = caption,
+    table.attr = 'class="table"'
+  )
+}
