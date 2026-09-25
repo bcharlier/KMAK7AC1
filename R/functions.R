f940616fe44932a910066a74e4d363c018f1f9eb
Benjamin Charlier <benjamin.charlier@inrae.fr>
2026-09-25 02:08:11 +0200

    chapter ACM is done

--- R/functions.R
+++ R/functions.R
@@ -1,4 +1,4 @@
-print_table <- function(data, digits = 3, caption = "") {
+print_table <- function(data, digits = 3, caption = "", scroll = 0) {
   data <- as.data.frame(data)
 
   if (digits >= 0) {
@@ -11,11 +11,20 @@ print_table <- function(data, digits = 3, caption = "") {
   }
 
   align <- rep("c", ncol(data))
-  knitr::kable(
+  
+  tbl <- knitr::kable(
     data,
     format = "html",
     align = align,
     caption = caption,
     table.attr = 'class="table" style="width: auto; max-width: 100%; margin: 0 auto; table-layout: auto;"'
   )
+
+  # Si scroll est un nombre positif, on applique la hauteur max en em
+  if (is.numeric(scroll) && scroll > 0) {
+    style <- sprintf("max-height: %sem; overflow-y: auto; overflow-x: auto; display: block;", scroll)
+    tbl <- htmltools::HTML(sprintf('<div style="%s">%s</div>', style, tbl))
+  }
+
+  return(tbl)
 }
